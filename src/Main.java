0ed92486b0722df8dfad0009bb2f21e55151f758
nagasairoopa <32008005+nagasairoopa@users.noreply.github.com>
2017-09-16 13:26:43 +0530

    Add files via upload

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class Main {
+
+	public static void main(String[] args) {
+int c;
+		//You may test that your code works find here
+		//Please check that your code works and has no
+		//compilation problems before to submit
+		StudentGroup stgrp = new StudentGroup(5);
+
+
+	  Date dt=new Date(1996/9/01);
+		Student obj = new Student(01,"sai",dt,73.02);
+		 stgrp.addFirst(obj);
+        stgrp.addLast(obj);
+		  System.out.println(stgrp.getStudent(0).getId());
+
+	}
+
+}
